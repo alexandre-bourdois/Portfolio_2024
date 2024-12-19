8be36454ef0ede38a78858cacb1db743711efd6d
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-12-19 18:18:08 +0100

    Fix resume button prb

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,8 +1,13 @@
+import Container from '@/components/container'
 import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <Container className='py-6 md:py-12 flex flex-col md:flex-row gap-6 md:gap-14'>
+      <div className='w-full md:w-2/3 '>Form</div>
+      <div className='w-full md:w-1/3 '>details</div>
+    
+    </Container>
   )
 }
 
