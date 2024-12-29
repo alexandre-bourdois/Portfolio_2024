b371f029780bc07240d7595b34d71efa2b271e3f
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-12-29 15:10:35 +0100

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -9,11 +9,11 @@ import Header from "@/components/header/header";
 import PageTransition from "@/components/transition/PageTransition";
 import StairTransition from "@/components/transition/StairTransition";
 import { Toaster } from "@/components/ui/toaster";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: "Portfolio | Alexandre Bourdois",
-  description: "Alexandre BOURDOIS's Porfolio.",
+  description: "Alexandre BOURDOIS's Portfolio.",
 };
 
 const raleway = localFont({
@@ -29,7 +29,6 @@ export default async function RootLayout({
   children: React.ReactNode;
   params: Promise<{ locale: string }>;
 }) {
-
   const { locale } = await params;
 
   if (!routing.locales.includes(locale as typeof routing.locales[number])) {
@@ -40,20 +39,40 @@ export default async function RootLayout({
 
   return (
     <html lang={locale}>
-      <body
-        className={`${raleway.variable} antialiased text-white`}
-      >
+      <body className={`${raleway.variable} antialiased text-white`}>
         <NextIntlClientProvider locale={locale} messages={messages}>
           <Header />
-          <StairTransition/>
+          <StairTransition />
           <PageTransition>
             <div className="px-10">
               {children}
             </div>
           </PageTransition>
-          <Toaster/>
+          <Toaster />
         </NextIntlClientProvider>
-        <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_TOKEN as string } />
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            `,
+          }}
+        />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
       </body>
     </html>
   );
