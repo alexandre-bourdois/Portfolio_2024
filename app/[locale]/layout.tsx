cd05ca5bfa761386f4853b5ce8206ac3ac617779
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2025-02-02 17:29:53 +0100

    Add speedInsights

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import PageTransition from "@/components/transition/PageTransition";
 import StairTransition from "@/components/transition/StairTransition";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
@@ -91,6 +92,7 @@ export default async function RootLayout({
           </PageTransition>
           <Toaster />
           <Analytics />
+          <SpeedInsights />
         </NextIntlClientProvider>
       </body>
     </html>
